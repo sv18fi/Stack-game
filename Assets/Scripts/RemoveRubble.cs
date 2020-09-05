ed84eb0ff052d7ba655ab9b71de931c192337453
sv18fi <70793718+sv18fi@users.noreply.github.com>
2020-09-05 13:16:07 -0400

    Add project

---
+++ Assets/Scripts/RemoveRubble.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveRubble : MonoBehaviour {
+
+
+	void OnCollisionEnter(Collision col)
+	{
+		Destroy(col.gameObject);
+	}
+}
